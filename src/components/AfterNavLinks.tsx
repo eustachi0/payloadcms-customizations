27c0e78b9f6a69ed738ac9658c5e3703949c0076
Eustachio <eustachio.dipaola@gmail.com>
2023-05-22 17:59:22 +0100

    playing with the right sidebar

--- src/components/AfterNavLinks.tsx
+++ src/components/AfterNavLinks.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+
+import "./test.scss";
 
 const AfterNavLinks: React.FC = () => {
-    return <div>AfterNavLinks</div>;
+    const [sideBarX, setSideBarX] = useState(null);
+    useEffect(() => {
+        setTimeout(() => {
+            setSideBarX(
+                document.querySelector(".collection-edit__sidebar-wrap")
+            );
+        }, 50);
+    });
+    const handleClick = () => {
+        const sidebar = document.querySelector(
+            ".collection-edit__sidebar-wrap"
+        );
+        // sidebar.className = "test";
+        // sidebar.style.backgroundColor = "red"
+        console.log(sidebar);
+    };
+
+    return (
+        <div>
+            {sideBarX !== null ? (
+                <button onClick={handleClick}>Testing</button>
+            ) : (
+                ""
+            )}
+
+            <div>AfterNavLinks</div>
+        </div>
+    );
 };
 
 export default AfterNavLinks;
